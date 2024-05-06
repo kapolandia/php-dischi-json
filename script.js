6164161777707fab1fd2349a001c6bf99c4fc4cb
kapolandia <paykapo4@gmail.com>
2024-05-06 17:21:07 +0200

    start

---
+++ script.js
@@ -0,0 +1,22 @@
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      cards: [],
+      message: 'Ciao',
+    }
+  },
+  methods: {
+    diskApiCall() {
+        axios.get('server.php', {})
+          .then(function (response) {
+            this.cards = response.data;
+            console.log(this.cards);
+          })
+    }
+  },
+  mounted() {
+    this.diskApiCall()
+  }
+}).mount('#app')
